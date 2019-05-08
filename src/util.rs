60c2f3c697188673404673f661a95b4fd30f7ebc
Philip Craig <philipjcraig@gmail.com>
2019-05-08 13:02:32 +1000

    Simplify write_align

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,8 @@
-use std::iter;
-
 pub(crate) fn align(offset: usize, size: usize) -> usize {
     (offset + (size - 1)) & !(size - 1)
 }
 
 pub(crate) fn write_align(buffer: &mut Vec<u8>, size: usize) {
-    let prev_offset = buffer.len();
-    let offset = align(prev_offset, size);
-    buffer.extend(iter::repeat(0).take(offset - prev_offset));
+    let offset = align(buffer.len(), size);
+    buffer.resize(offset, 0);
 }
